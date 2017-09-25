4e22f19504f948d7d270c1004d56374c21ab8fcc
Andrew Peck <olaf223224@gmail.com>
2017-09-25 17:48:20 -0400

    change app to stateless function
    state is for wieners

--- App.js
+++ App.js
@@ -2,15 +2,12 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import Styles from 'global-styles';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={ Styles.container }>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <Text>Hi, this is will</Text>
-      </View>
-    );
-  }
-}
+const App = () => (
+  <View style={ Styles.container }>
+    <Text>Open up App.js to start working on your app!</Text>
+    <Text>Changes you make will automatically reload.</Text>
+    <Text>Shake your phone to open the developer menu.</Text>
+  </View>
+);
+
+export default App;
