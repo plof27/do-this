809a81961471128abf2397bb76e4444424f59e70
Andrew Peck <olaf223224@gmail.com>
2017-10-06 15:21:01 -0400

    App waits for loading before rendering

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Container, Header, Content } from 'native-base';
 import AppFooter from './components/AppFooter';
-import { Font } from 'expo';
+import { Font, AppLoading } from 'expo';
 
 class App extends React.Component {
-  async componentWillMount() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      currentPage: 'list',
+    };
+  }
+
+  async _loadResourcesAsync() {
     await Font.loadAsync({
       'Roboto': require('native-base/Fonts/Roboto.ttf'),
       'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
@@ -12,6 +20,16 @@ class App extends React.Component {
   }
 
   render() {
+    if (this.state.loading) {
+      return (
+        <AppLoading
+          startAsync={ this._loadResourcesAsync }
+          onFinish={ () => this.setState({loading: false}) }
+          onError={ console.warn } // I have a vague idea of what this does...
+        />
+      );
+    }
+
     return(
       <Container>
         <Header />
